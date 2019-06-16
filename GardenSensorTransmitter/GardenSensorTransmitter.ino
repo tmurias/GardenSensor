0b72427cd06e865399ef96d86caabc0b6727c924
Lizzy Neale <Lizzy@w134-87-154-038.wireless.uvic.ca>
2019-06-16 14:50:56 -0700

    Added Transmitter sketch

---
+++ GardenSensorTransmitter/GardenSensorTransmitter.ino
@@ -0,0 +1,27 @@
+#include <RH_ASK.h>
+#include <SPI.h> // Not actually used but needed to compile
+
+RH_ASK driver;
+
+void setup()
+{
+    Serial.begin(9600);    // Debugging only
+    if (!driver.init())
+         Serial.println("init failed");
+}
+
+void loop()
+{
+
+    int val;
+    val = analogRead(A0); //connect sensor to Analog 0 Serial.print(val); //print the value to serial port delay(100);
+    uint8_t val_byte = (val+1)/4-1; //converting to byte (1023 -> 255)
+    
+    //const char *msg = "Hello 499 :)";
+    const uint8_t msg[12] = {val_byte,0,0,0,0,0,0,0,0,0,0,0}; //transmitter sends 12 bytes so only using the first value in this case
+    
+    
+    driver.send((uint8_t *)msg, strlen(msg));
+    driver.waitPacketSent();
+    delay(1000);
+}
