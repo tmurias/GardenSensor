9503a292f74e3478f0e2943576c36cfa244de0c4
Taylor Murias <taylor@murias.ca>
2019-07-29 17:33:35 -0700

    Added function stub for getting temperatures

--- GardenSensorTransmitter/GardenSensorTransmitter.ino
+++ GardenSensorTransmitter/GardenSensorTransmitter.ino
@@ -5,23 +5,32 @@ RH_ASK driver;
 
 void setup()
 {
-    Serial.begin(9600);    // Debugging only
-    if (!driver.init())
-         Serial.println("init failed");
+  Serial.begin(9600);    // Debugging only
+  if (!driver.init())
+    Serial.println("init failed");
+
+  uint8_t test[2];
+  get_temperatures(test);
 }
 
 void loop()
 {
 
-    int val;
-    val = analogRead(A0); //connect sensor to Analog 0 Serial.print(val); //print the value to serial port delay(100);
-    uint8_t val_byte = (val+1)/4-1; //converting to byte (1023 -> 255)
+  int val;
+  val = analogRead(A0); //connect sensor to Analog 0 Serial.print(val); //print the value to serial port delay(100);
+  uint8_t val_byte = (val+1)/4-1; //converting to byte (1023 -> 255)
     
-    //const char *msg = "Hello 499 :)";
-    const uint8_t msg[12] = {val_byte,0,0,0,0,0,0,0,0,0,0,0}; //transmitter sends 12 bytes so only using the first value in this case
+  //const char *msg = "Hello 499 :)";
+  const uint8_t msg[12] = {val_byte,0,0,0,0,0,0,0,0,0,0,0}; //transmitter sends 12 bytes so only using the first value in this case
     
     
-    driver.send((uint8_t *)msg, strlen(msg));
-    driver.waitPacketSent();
-    delay(1000);
+  driver.send((uint8_t *)msg, strlen(msg));
+  driver.waitPacketSent();
+  delay(1000);
+}
+
+void get_temperatures(uint8_t* values)
+{
+  values[0] = 1; // TODO: Get actual value
+  values[2] = 2; // TODO: Get actual value
 }
