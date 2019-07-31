d55473c1ad129da0f9a2f8358272bf175e00b902
Tristan Reed <tristan.reed543@gmail.com>
2019-07-31 11:31:30 -0700

    LCD formatting of receiver input code

--- GardenSensorReceiver/GardenSensorReceiver.ino
+++ GardenSensorReceiver/GardenSensorReceiver.ino
@@ -4,58 +4,49 @@
 
 RH_ASK driver;
 LiquidCrystal lcd(7, 8, 9, 10, 6 , 12);
-
+int i = 0;
 void setup()
 {
   Serial.begin(9600);  // Debugging only
   if (!driver.init())
     Serial.println("init failed");
+  lcd.begin(16, 2);
+  lcd.setCursor(0, 0);
+  lcd.write("Ts:   C Ta:   C");
+
+  //lcd.write("Ta:");
+  lcd.setCursor(0, 1);
+  lcd.write("Humidity:    %");
 }
 
 void loop()
 {
 
+
   uint8_t buf[12];
+
   uint8_t buflen = sizeof(buf);
   if (driver.recv(buf, &buflen)) // Non-blocking
   {
+    // (col,row)
+    Serial.print(buf[0] );
+    Serial.print( buf[1] );
+    Serial.print( buf[2] );
+// TODO if statement for single digit to double digit humidity level change % position
+    lcd.setCursor(10, 1);
+    char Humidity [4];
+    itoa(25, Humidity, 10);
+    lcd.write({Humidity});
 
-    //TODO: confirm order of cursor (row, col) or (col,row)
-    lcd.begin(16, 2);
-    lcd.setCursor(0, 0);
-    lcd.write("Ts: ");
-    lcd.setCursor(0, 9)
-    lcd.write("Ta:");
-    lcd.setCursor(1, 0);
-    lcd.write("Humidity:");
-    lcd.setCursor(1, 14);
-    lcd.write("%");
-        lcd.setCursor(0, 4)
-        lcd.write(buf[0])
-      }
+    lcd.setCursor(4, 0);
+    char ts [2];
+    itoa(buf[1], ts, 10);
+    lcd.write({ts});
 
-      if (i == 1) {
-        lcd.setCursor(0, 12)
-        lcd.write(buf[1])
-      }
+    lcd.setCursor(12, 0);
+    char ta [2] ;
+    itoa(buf[2], ta, 10);
+    lcd.write({ta});
 
-      if (i == 2) {
-        lcd.setCursor(1, 10)
-        lcd.write(buf[2])
-      }
-    }
-
-
-
-    // Message with a good checksum received, display it
-
-
-
-    //lcd.setCursor(3, 0);
-    //Serial.println(buf[0]);
-
-    //  char hum_val[3];
-    //itoa(buf[0], hum_val, 10);
-    //lcd.write(hum_val);
   }
 }
